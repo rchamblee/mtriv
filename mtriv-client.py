a344cb29d8a09fd50f0d855574cbc0131f8d69b2
Rickey Chamblee <rickeyachamblee@gmail.com>
2018-07-24 21:18:26 -0500

    Added initial release

---
+++ mtriv-client.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+import socket
+from time import sleep
+from threading import Thread
+import sys
+import shutil
+
+TCP_IP = 'localhost'
+TCP_PORT =  80
+BUFFER_SIZE = 1024
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sIP = TCP_IP
+sP = TCP_PORT
+try:
+	if(sys.argv[1]):
+		sIP = sys.argv[1]
+		if(sys.argv[2]):
+			sP = sys.argv[2]
+except:
+	sIP = input("What's the IP then?: ")
+	sP = int(input("Oi, and the port?"))
+s.connect((sIP, int(sP)))
+print("Who's you?")
+nick = input()        
+
+s.sendto(nick.encode('utf-8'), (TCP_IP,TCP_PORT))
+
+height = shutil.get_terminal_size().lines - 1
+stdout_write_bytes = sys.stdout.buffer.write
+CSI = b'\x1b['
+CLEAR = CSI + b'2J'
+CLEAR_LINE = CSI + b'2K'
+SAVE_CURSOR = CSI + b's'
+UNSAVE_CURSOR = CSI + b'u'
+
+GOTO_INPUT = CSI + b'%d;0H' % (height + 1)
+
+def emit(*args):
+    stdout_write_bytes(b''.join(args))
+
+def set_scroll(n):
+    return CSI + b'0;%dr' % n
+
+emit(CLEAR, set_scroll(height))
+    
+def getData(sock):
+    timecounter = 0
+    
+    while True:
+        data_raw = s.recv(BUFFER_SIZE)
+        data = data_raw.decode('utf-8')
+        if not data:
+            break
+        if (data.endswith("\r\n")):
+            lines = data.split("\r\n")
+            for line in lines:
+                print (line)
+        else:
+            print(data)
+        timecounter += 1
+        if((timecounter % 20) == 0):
+            sock.send(('ping').encode('utf-8'))
+            sleep(0.25)
+            try:
+                response = sock.recv(1024).decode('utf-8')
+            except:
+                if(not response):
+                    print("Connection DED :c. Attempting reconnect...")
+                    try:
+                        s.connect((TCP_IP,TCP_PORT))
+                    except:
+                        print("unable to recover connection to " + TCP_IP + " quitting....")
+                else:
+                    print("Unknown error occured.... dumping $response\n" + response)
+                    
+
+t = Thread(target=getData, args=(s,))
+t.daemon = True
+t.start()
+
+
+try:
+    while True:
+        emit(SAVE_CURSOR, GOTO_INPUT, CLEAR_LINE)
+        try:
+            data = input()
+            if(data == "/quit"):
+                emit(UNSAVE_CURSOR)
+                break
+            s.sendto(data.encode('utf-8'), (TCP_IP,TCP_PORT))
+        except:
+            continue
+        finally:
+            emit(UNSAVE_CURSOR)
+
+except KeyboardInterrupt:
+    #Disable scrolling, but leave cursor below the input row
+    emit(set_scroll(0), GOTO_INPUT, b'\n')
+    
---
+++ mtriv-server.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+import socket
+from threading import Thread
+import time
+from time import sleep
+import random
+
+TCP_IP = '172.17.0.2'
+TCP_PORT =  80
+BUFFER_SIZE = 1024
+
+new_data = ''
+new_data_src = ''
+scorePairs=[]
+qaPairs = []
+class Question:
+    def __init__(self,prompt,answer):
+        self.prompt = prompt
+        self.answer = answer.upper()
+        self.solved = False
+
+class ClientThread(Thread):
+    def __init__(self,ip,port,sock,nick):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        self.handled = False
+        self.data = ''
+        self.nick = nick
+        self.score = 0
+        print ("New thread for: " + ip + ":" + str(port))
+        msgClients("New user connected: " + self.nick)
+        
+    def run(self):
+        global new_data, new_data_src,scorePairs
+        newPair = (self.nick,self.score)
+        scorePairs.append(newPair)
+        while True:
+            rawdata = self.sock.recv(BUFFER_SIZE)
+            curtime = time.strftime ("%H:%M")
+            self.data = rawdata.decode('utf-8')
+            if (not (self.data and self.data.strip())):
+                break
+            if(self.data == "ping"):
+                print("Received ping from " + self.ip)
+                response = "pong"
+                self.sock.send(response.encode('utf-8'))
+            elif(self.data == "!startq"):
+                qThread = Thread(target=beginQuestion,args=())
+                qThread.start()
+            elif(self.data == "!stopq"):
+                qThread.stop()
+            elif(self.data == "!scores" or self.data == "!score"):
+                msgClients("Scores----------")
+                for pair in scorePairs:
+                    msgClients(pair[0] + ": " + str(pair[1]))
+                    sleep(0.2)
+            else:
+                new_data = self.data
+                new_data_src = self.nick
+                for thread in threads:
+                    try:
+                        thread.sock.send(("["+curtime+"]" + self.nick + ': ' + self.data).encode('utf-8'))
+                    except:
+                        print("Closing thread for " + ip + ": Connection broken")
+                        return 1
+
+def msgClients(message):
+    for thread in threads:
+        thread.sock.send(("["+time.strftime("%H:%M")+"][Multitriv]: " + message).encode('utf-8'))
+
+
+            
+        
+def solveCheck(Question):
+    global new_data,scorePairs
+    while(not Question.solved):
+        if(new_data == Question.answer):
+            Question.solved = True
+            msgClients("Question has been solved! Solved by: " + new_data_src)
+            new_data = ''
+            for pair in scorePairs:
+                if(pair[0] == new_data_src):
+                    scorePairs.insert(0,(pair[0],pair[1] + 1))
+                    scorePairs.remove(pair)
+                    print(pair[0] + " scored")
+            break
+        if(new_data == "!skip"):
+            new_data = ''
+            msgClients("Question skipped.")
+            return 1
+        sleep(0.5)
+    return 1
+    
+        
+def beginQuestion():
+    while(True):
+        quest = random.choice(qaPairs)
+        print("Initiating new question...")
+        msgClients(quest.prompt)
+        solveCheck(quest)
+        msgClients("Next question in 5s....")
+        sleep(5)
+
+def loadqDoc(filename):
+    import re
+    global qaPairs
+    try:
+        with(open(filename,"r")) as f:
+            for line in f.readlines():
+                elems = line.split(":")
+            for index,item in enumerate(elems):
+                newQ = Question('a','b')
+                if(re.match(r'^[0-9]*\.',item)):
+                      newQ.prompt = item
+                      newQ.answer = elems[index + 1]
+                      qaPairs.append(newQ)
+    finally:
+        print("done loading questions from " + filename)
+
+        
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind((TCP_IP, TCP_PORT))
+threads = []
+
+loadqDoc("qdoc.mtq")
+
+while True:
+    tcpsock.listen(5)
+    print ("Waiting for incoming connections...")
+    (conn, (ip,port)) = tcpsock.accept()
+    conn_Nick = conn.recv(1024).decode('utf-8')
+    print ('Got connection from ', (ip,port,conn_Nick))
+    newthread = ClientThread(ip,port,conn,conn_Nick)
+    newthread.start()
+    threads.append(newthread)
+    for thread in threads:
+        if not thread.isAlive():
+            thread.handled = True
+    threads = [thread for thread in threads if not thread.handled]
+        
+
+for thread in threads:
+    thread.join()
+
